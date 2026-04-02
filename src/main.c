17e9b8b7bad24843ee3075cc108b51bc207a030d
zero-volts <zero.volts.lab@gmail.com>
2026-04-02 09:54:26 -0300

    Termiando de leer los servicios y lectura de cacracteristicas del servicio

--- src/main.c
+++ src/main.c
@@ -17,12 +17,47 @@
 
 #include "esp_gattc_api.h"
 
-#define GATT_APPLICATION_ID 0
-#define SCAN_DURATION_IN_SEC 15
-#define DEFAULT_NAME "Unknown"
+#define GATT_APPLICATION_ID     0
+#define SCAN_DURATION_IN_SEC    15
+#define DEFAULT_NAME            "Unknown"
+#define MAX_SERVICES            20
 
 static const char *TAG = "ZEROVOLTS_FIRMWARE";
 static esp_gatt_if_t gatt_if = ESP_GATT_IF_NONE;
+static zv_bt_service_t discovered_services[MAX_SERVICES];
+static int services_count = 0;
+
+static zv_bt_service_t *find_service(const esp_bt_uuid_t uuid)
+{
+    for (int index = 0; index < services_count; index++)
+    {
+        if ( discovered_services[index].uuid.uuid.uuid32 == uuid.uuid.uuid32)
+            return &discovered_services[index];
+    }
+
+    return NULL;
+}
+
+static void zv_bt_add_services(zv_bt_service_t new_service)
+{
+    zv_bt_service_t *service = find_service(new_service.uuid);
+    if (service != NULL)
+        return;
+    
+    if (services_count >= MAX_SERVICES)
+    {
+        ESP_LOGI(TAG, "Can't save more services, is in the limit of %d", MAX_SERVICES);
+        return;
+    }
+
+    service = &discovered_services[services_count++];
+
+    service->uuid = new_service.uuid;
+    service->start_handle = new_service.start_handle;
+    service->end_handle = new_service.end_handle;
+
+    ESP_LOGI(TAG, "service stored ID: %d", new_service.uuid.uuid.uuid32);
+}
 
 static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
 {    
@@ -93,6 +128,7 @@ static void gatt_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_i
     {
         case ESP_GATTC_REG_EVT:
 
+            // 1.- Application id registered and storing the interface GATT id 
             if (param->reg.status == ESP_GATT_OK) 
             {    
                 gatt_if = gattc_if;
@@ -109,11 +145,14 @@ static void gatt_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_i
             break;
        
         case ESP_GATTC_OPEN_EVT:
+            // 2.- Virtual connection is open
             ESP_LOGI(TAG, "gatt_event_handler::ESP_GATTC_OPEN_EVT ");
             ESP_LOGI(TAG, "gatt_event_handler::ESP_GATTC_OPEN_EVT status: %d", param->open.status);
             if (param->open.status == ESP_GATT_OK)
             {
                 ESP_LOGI(TAG, "gatt_event_handler::ESP_GATTC_OPEN_EVT connection ID: %d", param->open.conn_id);
+                
+                // Making the transmission buffer bigger.
                 esp_ble_gattc_send_mtu_req(gatt_if, param->open.conn_id);  
             }
             
@@ -125,10 +164,50 @@ static void gatt_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_i
             esp_ble_gattc_search_service(gatt_if, param->cfg_mtu.conn_id, NULL);
             break;
         case ESP_GATTC_SEARCH_RES_EVT:
-            ESP_LOGI(TAG, "gatt_event_handler::ESP_GATTC_SEARCH_RES_EVT:: srvc_id: %d ", param->search_res.srvc_id.uuid.uuid.uuid16);
+            // service(s) found
+
+            zv_bt_service_t new_service;
+            new_service.uuid = param->search_res.srvc_id.uuid;
+            new_service.start_handle = param->search_res.start_handle;
+            new_service.end_handle = param->search_res.end_handle;
+
+            zv_bt_add_services(new_service);
+            
             break;
         case ESP_GATTC_SEARCH_CMPL_EVT:
             ESP_LOGI(TAG, "gatt_event_handler::ESP_GATTC_SEARCH_CMPL_EVT");
+            // No more services found getting the characteristic from the services.
+
+            ESP_LOGI(TAG, "gatt_event_handler::services found amount : %d", services_count);
+            for (int index = 0; index < services_count; index++)
+            {
+                esp_gattc_char_elem_t *characteristic = malloc (sizeof (esp_gattc_char_elem_t));
+                uint16_t count = 1;
+                zv_bt_service_t service = discovered_services[index];
+                esp_gatt_status_t result = esp_ble_gattc_get_all_char(gatt_if, 
+                    param->search_cmpl.conn_id, 
+                    service.start_handle, 
+                    service.end_handle, 
+                    characteristic, 
+                    &count, 
+                    0
+                );
+
+                ESP_LOGI(TAG, "gatt_event_handler::result : %d", result);
+                if (result == ESP_OK)
+                {
+                    ESP_LOGI(TAG, "gatt_event_handler::services found : %d", characteristic->char_handle);
+                    ESP_LOGI(TAG, "gatt_event_handler::read property : %d", characteristic->properties & ESP_GATT_CHAR_PROP_BIT_READ);
+                    ESP_LOGI(TAG, "gatt_event_handler::write nr property : %d", characteristic->properties & ESP_GATT_CHAR_PROP_BIT_WRITE_NR);
+                    ESP_LOGI(TAG, "gatt_event_handler::write property : %d", characteristic->properties & ESP_GATT_CHAR_PROP_BIT_WRITE);
+                    ESP_LOGI(TAG, "gatt_event_handler::notify property : %d", characteristic->properties & ESP_GATT_CHAR_PROP_BIT_NOTIFY);
+                    ESP_LOGI(TAG, "gatt_event_handler::notify indicate : %d", characteristic->properties & ESP_GATT_CHAR_PROP_BIT_INDICATE);
+                }
+               
+                free(characteristic);
+                break;
+            }
+
             break;
         default:
             ESP_LOGI(TAG, "gatt_event_handler:: event not recognized: %d", event);
@@ -159,20 +238,36 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_bluedroid_init());
     ESP_ERROR_CHECK(esp_bluedroid_enable());
 
+    // TODO: mejorar diagrama, crear un diagrama de flujo para entender mejor y estar seguro de cadada llamada
     // 1.- Configuracion del host y controller
     // 2.- Registro del handler del GAP (esp_ble_gap_register_callback)
     // 3.- Seteando los paramatros para el escaneo (esp_ble_gap_set_scan_params)
     //     |
     //     |------> ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT
     //     |        |
-    //     |        |-------> esp_ble_gap_start_scanning
+    //     |        |-------> Se da inicio para buscar o escuchar dispositivos esp_ble_gap_start_scanning
     //     |
     //     |------> .....
-    //     |------> Termina el scan llamando el evento ESP_GAP_SEARCH_INQ_CMPL_EVT
+    //     |------> Termina el scan y se llama el evento ESP_GAP_SEARCH_INQ_CMPL_EVT
     //             |
     //             |--------> Se obtiene el dispositivo mas cercano y se abre la comunicacion con el GATT
     //                        llamando a esp_ble_gattc_open
-        
+    //                        |
+    //                        |--------> ESP_GATTC_CONNECT_EVT
+    //                        |--------> ESP_GATTC_OPEN_EVT
+    //                                   |
+    //                                   |---------> esp_ble_gattc_send_mtu_req
+    //                                               |
+    //                                               |----> ESP_GATTC_CFG_MTU_EVT
+    //                                                      |
+    //                                                      |------>  Se comienza a buscar servicios del dispositivo usando 
+    //                                                                esp_ble_gattc_search_service
+    //                                                                |
+    //                                                                |---> ESP_GATTC_SEARCH_RES_EVT
+    //                                                                |
+    //                                                                |----> ESP_GATTC_SEARCH_CMPL_EVT
+    //
+    //    
     // 4.- Registro del handler para GATTC (esp_ble_gattc_register_callback)
     // 5.- Registro de identificacion de la aplicacion (esp_ble_gattc_app_register)
     //     |
